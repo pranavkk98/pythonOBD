0be140530b977e1f4968e765af3fdabfa540195f
Pranav <pranavkochhar3198@gmail.com>
2024-11-29 21:42:13 +0530

    updations and cleanup

--- app/obd_service.py
+++ app/obd_service.py
@@ -1,5 +1,26 @@
 from urllib import response
 import obd
+import serial
+
+def connect_to_obd(port):
+    global connection
+    if connection is not None:
+        return connection
+    try:
+        connection = obd.OBD(port=port)  # Initialize connection on the given port
+        return connection
+    except serial.SerialException:
+        return None
+
+# Function to disconnect from OBD-II
+def disconnect_obd():
+    global connection
+    if connection:
+        connection.close()
+        connection = None
+        return True
+    return False
+
 def get_engine_voltage():
   connection = obd.OBD() 
   voltage_cmd = obd.commands.GET_DTC
--- app/routes.py
+++ app/routes.py
@@ -1,14 +1,38 @@
+from urllib import request
 from flask import Blueprint, jsonify
+
+from OBDApp.app import obd_service
 #from .obd_service import get_engine_voltage, get_speed
-from .emulator_service import initialize_elm, get_engine_voltage
 
 main = Blueprint('main', __name__)
 
 
-@main.route('/engine_voltage', methods=['GET'])
-def engine_voltage():
-    voltage = get_engine_voltage()
-    return jsonify({"engine_voltage": voltage})
+@main.route('/connect', methods=['POST'])
+def connect():
+    data = request.json
+    bt_device = data.get('port')
+    
+    connection = obd_service.connect_to_obd(bt_device)
+
+    if connection:
+        return jsonify({"status": "connected", "port": bt_device})
+    else:
+        return jsonify({"status": "failed", "message": "Could not connect to OBD-II device"}), 500
+
+# @main.route('/engine_voltage', methods=['GET'])
+# def engine_voltage():
+#     voltage = get_engine_voltage()
+#     return jsonify({"engine_voltage": voltage})
+
+@main.route('/disconnect', methods=['POST'])
+def disconnect():
+    success = obd_service.disconnect_obd()
+    if success:
+        return jsonify({"status": "disconnected"})
+    else:
+        return jsonify({"status": "error", "message": "No active connection to disconnect"}), 400
+
+    
 
 
 #@main.route('/get_speed')
